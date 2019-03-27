0862aa3febae9662822321cab3938cb898533b8a
kaizhilam <kaizhilam@gmail.com>
2019-03-27 19:39:48 +1100

    change in player and camera movement
    Improved character moving and 3rd person camera movement.
    Also added ray tracing for aiming

--- SIT_302_374 _T1_2019/Assets/Scripts for movement/ThirdPersonCamera.cs
+++ SIT_302_374 _T1_2019/Assets/Scripts for movement/ThirdPersonCamera.cs
@@ -4,25 +4,90 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public Transform LookAt;
+    public Transform FocusOn;
     public float Distance;
-    private float currentX = 0.0f;
-    private float currentY = 0.0f;
+    public GameObject LookingAtGameObject { get; private set; } //to allow player to interact with object that the camera is looking at (pick up object)
+    public Vector3 LookingAtPoint { get; private set; } //allow player to interact with object that the camera is looking at (shooting)
+    public float LookingAtDistance { get; private set; } //allow player to interact with object that the camera is looking at (calculate distance)
+    public float MouseSensitivity;
+
+    private float _CurrentX = 0.0f;
+    private float _CurrentY = 0.0f;
+    private float _CurrentDistance;
+
+    private void Start()
+    {
+        _CurrentDistance = Distance;
+    }
 
     private void Update()
     {
-        currentY = Mathf.Clamp(currentY + Input.GetAxis("Mouse Y"), 1, 70);
-        currentX += Input.GetAxis("Mouse X");
-
+        //CameraZooming();
+        CameraMovement();
+        CameraAiming();
     }
 
-    private void LateUpdate()
+    private void CameraMovement()
     {
-        Vector3 dir = new Vector3(0, 0, -Distance); //set distance between LookAt and camera
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
-        transform.position = LookAt.position + rotation * dir; //rotate camera around player
-        transform.LookAt(LookAt); //camera face player
-        LookAt.transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); //rotate player
+        _CurrentX += Input.GetAxis("Mouse X");
+        _CurrentY += Input.GetAxis("Mouse Y");
+        _CurrentY = Mathf.Clamp(_CurrentY, 1, 89); //so the y axis does not clip through ground
+
+        Vector3 dir = new Vector3(0, 0, -_CurrentDistance); //set distance between LookAt and camera
+        Quaternion rotation = Quaternion.Euler(_CurrentY * MouseSensitivity, _CurrentX * MouseSensitivity, 0);
+        transform.position = FocusOn.position + rotation * dir; //rotate camera around player
+        transform.LookAt(FocusOn); //camera face player
+        transform.Translate(new Vector3(1f, 1f, 0f)); //move the camera slightly to the top right so camera ray casting wont keep hitting the player
+        FocusOn.transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z); //rotate lookAt object
     }
 
+    private void CameraAiming() //Raycasting for character interaction with objects
+    {
+        RaycastHit hit;
+        Ray objectRay = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.red);
+        if (Physics.Raycast(objectRay, out hit))
+        {
+            LookingAtGameObject = hit.collider.gameObject;
+            LookingAtPoint = hit.point;
+            LookingAtDistance = hit.distance;
+            //Debug.Log("Name: " + hit.collider.name + " Point: " + hit.point + " Distance: " + hit.distance);
+        }
+        else
+        {
+            LookingAtGameObject = null;
+            LookingAtPoint = Vector3.positiveInfinity;
+        }
+    }
+
+    //private void CameraZooming() //FIX THIS, camera won't stop stuttering when zoomed in at a specific angle, the camera is meant to zoom in so the player stays in the picture
+    //{
+    //    RaycastHit hitFront, hitBack;
+    //    Ray frontRay = new Ray(transform.position, FocusOn.position-transform.position);
+    //    Ray backRay = new Ray(transform.position, -1 * (FocusOn.position - transform.position));
+    //    //Debug.DrawRay(transform.position, FocusOn.position - transform.position, Color.black);
+    //    //Debug.DrawRay(transform.position, -1*(FocusOn.position - transform.position), Color.blue);
+    //    if (Physics.Raycast(frontRay, out hitFront))
+    //    {
+    //        if (hitFront.collider.tag != "Player")
+    //        {
+    //            _CurrentDistance -= 0.1f;
+    //        }
+    //        else
+    //        {
+    //            if (Physics.Raycast(backRay, out hitBack))
+    //            {
+    //                if (hitBack.distance > Distance)
+    //                {
+    //                    _CurrentDistance += 0.1f;
+    //                }
+    //            }
+    //            else if (_CurrentDistance < Distance)
+    //            {
+    //                _CurrentDistance += 0.1f;
+    //            }
+
+    //        }
+    //    }
+    //}
 }
