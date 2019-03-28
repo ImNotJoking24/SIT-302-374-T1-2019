374cd510904fb791af78a35b6953ded410c621d0
kaizhilam <kaizhilam@gmail.com>
2019-03-28 15:19:05 +1100

    Added projectile shooting mechanics
    not completed yet though

---
+++ SIT_302_374 _T1_2019/Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Projectile Projectile;
+    public ProjectileScriptableObject Type;
+
+    private bool _CanShoot = true;
+    private float _CanShootTimer = 0;
+
+    private void Update()
+    {
+        _CanShootTimer += Time.deltaTime;
+        if (_CanShootTimer >= Type.RechargeTimer)
+        {
+            _CanShoot = true;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        float fire = Input.GetAxisRaw("Fire1");
+        if (fire == 1 && Projectile != null && _CanShoot == true)
+        {
+            _CanShoot = false;
+            _CanShootTimer = 0;
+            Instantiate(Projectile, transform.position + new Vector3(0f, 0f, 1f), transform.rotation);
+        }
+    }
+}
---
+++ SIT_302_374 _T1_2019/Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    public float HP;
+    public ProjectileScriptableObject ProjectileAbility;
+}
